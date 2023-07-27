260e79776ea4757887e8383c9ad58321f02f0db6
kangin wife <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-07-27 18:41:15 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,16 +1,6 @@
+import re
+
 def solution(my_string):
     answer = ''
-    for i in range(0, len(my_string)):
-        if my_string[i] == 'a':
-            continue
-        elif my_string[i] == 'e':
-            continue
-        elif my_string[i] == 'u':
-            continue
-        elif my_string[i] == 'o':
-            continue
-        elif my_string[i] == 'i':
-            continue
-        else:
-            answer += my_string[i]
+    answer = re.sub("a|e|i|o|u","",my_string)
     return answer
