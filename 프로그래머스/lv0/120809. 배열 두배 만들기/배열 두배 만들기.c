98ce3421d67e9721cf87f0e1e9e792256296c62f
지은입니다 <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-08-24 22:08:34 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.27 ms, Memory: 4.47 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int* solution(int numbers[], size_t numbers_len) {
+    int* answer = (int*)malloc(sizeof(int)*numbers_len);
+    for(int i = 0; i<numbers_len; i++){
+        answer[i] = numbers[i]*2;
+    }
+    return answer;
+}
