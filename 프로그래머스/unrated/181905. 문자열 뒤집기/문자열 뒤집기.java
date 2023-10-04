10bb50d9c3c98ec68fb57c0e3afaa69da6c620c2
지은입니다 <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-10-04 18:57:31 +0900

    [unrated] Title: 문자열 뒤집기, Time: 10.81 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+            if(e<my_string.length()){
+                for(int i = 0; i<s; i++){
+                answer += my_string.charAt(i);
+            }
+            for(int i = e; i>=s; i--){
+                answer += my_string.charAt(i);
+            }
+            for(int i = e + 1; i<my_string.length(); i++){
+                answer += my_string.charAt(i);
+            }
+        }
+        else {
+            for(int i = 0; i<s; i++){
+                answer += my_string.charAt(i);
+            }
+            for(int i = e; i>=s; i--){
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        return answer;
+    }
+}
