787840b045aa35da9f13b8c8def84d7bd09858b5
지은입니다 <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-12-18 08:15:05 +0900

    [level unrated] Title: 부분 문자열, Time: 0.04 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -3,14 +3,14 @@ class Solution {
         int answer = 0;
         int len = (str1.length() == str2.length()) ? 1 : str2.length() - str1.length();
         
-        // str2 = 5, str1 = 2, len = 3, i = 0, 1, 2
-        // (0, 2) ,, (2, 4)
+        if(str2.substring(len).equals(str1)) answer = 1;
+        
         for(int i = 0; i<len; i++){
             if(str2.substring(i, i + str1.length()).equals(str1)){
                 answer = 1;
                 break;
             }
-            if(str2.substring(len).equals(str1)) answer = 1;
+            
         }
         return answer;
     }
