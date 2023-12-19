14eb53d58033eac5d73ca601b88efd73991e6566
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-20 08:34:07 +0900

    [level unrated] Title: 세 개의 구분자, Time: 45.94 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,16 +1,8 @@
 import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
-        String s[] = myStr.split("[abc]");
-        ArrayList <String> list = new ArrayList();
-        for(String str : s){
-            if(str.length() > 0) list.add(str);
-        }
-        if(list.size() == 0) list.add("EMPTY");
-        String[] answer = new String[list.size()];
-        int n = 0;
-        for(String str : list) answer[n++] = str;
-        
-        return answer;
+        String[] arr = Arrays.stream(myStr.split("[abc]")).filter(str -> !str.isEmpty()).toArray(String[]::new);
+
+        return arr.length == 0 ? new String[] {"EMPTY"} : arr;
     }
 }
