5228b9737c2c8048227d37fb4c5d19523e0a6d5e
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-24 08:22:38 +0900

    [level unrated] Title: 모스부호 (1), Time: 8.81 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] morseVal = letter.split(" ");
+        
+        for(int i = 0; i<morseVal.length; i++){
+            for(int j = 0; j<morse.length; j++){
+                if(morseVal[i].equals(morse[j])) answer += (char)(j + 97);
+            }
+        }
+        
+        return answer;
+    }
+}
