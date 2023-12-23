4e8bfa815df01923d688f3e55ff7d592a65a3dfe
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-24 08:30:58 +0900

    [level unrated] Title: k의 개수, Time: 375.38 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.java
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.java
@@ -1,13 +1,7 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        String strK = Integer.toString(k);
-        for(int start = i; start<=j; start++){
-            String[] str = Integer.toString(start).split("");
-            for(int strLen = 0; strLen<str.length; strLen++){
-                if(str[strLen].equals(strK)) answer++;
-            }
-        }
-        return answer;
+        String answer = "";
+        for(int a = i; a<=j; a++) answer += a + "";
+        return answer.length() - answer.replace(k+"", "").length();
     }
 }
