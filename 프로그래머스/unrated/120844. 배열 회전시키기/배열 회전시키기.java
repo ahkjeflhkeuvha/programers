67952b4950d5c2f1d7af2ceee8eda16b72ce6173
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-24 08:38:14 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        if(direction.equals("right")){
+            answer[0] = numbers[numbers.length - 1];
+            for(int i = 0, n = 1; i<numbers.length - 1; i++)
+                answer[n++] = numbers[i];
+        }
+        else {
+            for(int i = 1, n = 0; i<numbers.length; i++)
+                answer[n++] = numbers[i];
+            answer[numbers.length - 1] = numbers[0];
+        }
+        return answer;
+    }
+}
