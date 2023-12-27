3259d119ca3497ee0527b15732ba50d3cc27369b
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-28 07:45:43 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.09 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,19 +1,14 @@
+import java.util.Arrays;
 class Solution {
     public String[] solution(String[] str_list) {
-        int start = 0, end = 0;
         for(int i = 0; i<str_list.length; i++) {
             if(str_list[i].matches("r")) {
-                start = i+1; end = str_list.length;
-                break;
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
             else if(str_list[i].matches("l")){
-                start = 0; end = i;
-                break;
+                return Arrays.copyOfRange(str_list, 0, i);
             }
         }
-        String[] answer = new String[end - start];
-        int n = 0;
-        for(int i = start; i < end; i++) answer[n++] = str_list[i];
-        return answer;
+        return new String[0];
     }
 }
