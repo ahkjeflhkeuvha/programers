8f45c948e1f312536e9ad3dfdc8e22d1a6e2d81e
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-30 08:25:35 +0900

    [level unrated] Title: 옹알이 (1), Time: 1.16 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i = 0; i<babbling.length; i++){
+            babbling[i] = babbling[i].replaceFirst("aya", "0");
+            babbling[i] = babbling[i].replaceFirst("ye", "0");
+            babbling[i] = babbling[i].replaceFirst("woo", "0");
+            babbling[i] = babbling[i].replaceFirst("ma", "0");
+            babbling[i] = babbling[i].replaceAll("0", "");
+            if(babbling[i].equals("")) answer++; 
+        }
+        return answer;
+    }
+}
