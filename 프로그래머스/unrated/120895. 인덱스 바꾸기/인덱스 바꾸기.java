399a22b043e21385f48decfce346e59ce14c6277
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-30 07:30:48 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 1.69 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        String strArr[] = my_string.split("");
+        String temp = strArr[num1];
+        strArr[num1] = strArr[num2];
+        strArr[num2] = temp;
+        
+        for(String str : strArr) answer += str;
+        return answer;
+    }
+}
