894810f2b8c542334223db88c49047ab6eafc6fb
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-31 08:53:52 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         for(int i = 1; i<=n; i++){
             if(i%3 == 0 || Integer.toString(i).contains("3")) n++;
         }
-        return answer = n;
+        return n;
     }
 }
