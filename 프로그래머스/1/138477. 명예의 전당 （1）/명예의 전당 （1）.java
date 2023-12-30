917dd05081ca3a211599d5cdecb7f2b2f7c572b7
지은입니다 <w2317@e-mirim.hs.kr>
2023-12-31 08:08:30 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.21 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList <Integer> list = new ArrayList<>();
+        for(int i = 0; i<score.length; i++){
+            list.add(score[i]);
+            Collections.sort(list);
+            if(i - k < 0) answer[i] = list.get(0);
+            else answer[i] = list.get(i - k + 1);
+        }
+        return answer;
+    }
+}
