e01d9bb15f4bb21e05609a3f3e98c1bbb5924224
지은입니다 <w2317@e-mirim.hs.kr>
2024-01-03 08:27:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+   public int solution(int[] nums) {
+      int answer = 0;
+      boolean check = false;
+
+      for (int i = 0; i < nums.length; i++) {
+         for (int j = i + 1; j < nums.length; j++) {
+            for (int k = j + 1; k < nums.length; k++) {
+               int num = nums[i] + nums[j] + nums[k]; //값을 담아
+               if (num >= 2) check = decimal(num);
+               if (check) answer++;
+            }
+         }
+      }
+      return answer;
+   }
+   
+   public boolean decimal(int num) {
+      boolean check = true; 
+      if(num==2)  return check;
+      for(int i=2; i<num; i++) {
+         if(num%i ==0) {
+            check = false; break;
+         }
+      }
+      return check;
+   }
+}
