7142edff7babe42b13763087cf1ccc3b69d249b2
지은입니다 <w2317@e-mirim.hs.kr>
2024-01-07 22:02:21 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        // 최대공약수
+        for(int i = 1; i<=m; i++){
+            if(n%i==0 && m%i==0) answer[0] = i;
+        }
+        // 최소공배수
+        answer[1] = (n*m)/answer[0];
+        return answer;
+    }
+}
