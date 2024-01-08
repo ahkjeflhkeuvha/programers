10c6eb9d70ce41382a4ab986d958811448412861
지은입니다 <w2317@e-mirim.hs.kr>
2024-01-09 08:50:40 +0900

    [level unrated] Title: 등수 매기기, Time: 0.01 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[][] score) {
+        double[] avg = new double[score.length];
+        for(int i = 0; i<score.length; i++){
+            avg[i] = (double)(score[i][0] + score[i][1])/2.0;
+        }
+        
+        int[] answer = new int[score.length];
+        
+        for(int i = 0; i<score.length; i++){
+            int rank = 1;
+            for(int j = 0; j<score.length; j++){
+                if(avg[i] < avg[j]) rank++;
+                answer[i] = rank;
+            }
+        }
+        return answer;
+    }
+}
