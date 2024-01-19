09a1af0937737802d428dc6e92120b9c1f2391d0
지은입니다 <w2317@e-mirim.hs.kr>
2024-01-19 10:56:42 +0900

    [level 0] Title: 옹알이 (1), Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,8 +1,13 @@
 function solution(babbling) {
     var answer = 0;
-
+    var text = ['ma', 'woo', 'aya', 'ye'];
+    
     for(let i = 0; i<babbling.length; i++){
-        if(babbling[i].match("^(aya|ye|woo|ma)+$")) answer++;
+        for(let j = 0; j<text.length; j++){
+            babbling[i] = babbling[i].replace(text[j], ' ');
+        }
+        if(babbling[i].trim() == 0) answer++;
     }
+    
     return answer;
 }
