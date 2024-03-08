361c1774c154f218a4f7d0e5c30e55af2b7eecf4
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-08 19:53:13 +0900

    [level 1] Title: 추억 점수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,9 +1,10 @@
 function solution(name, yearning, photo) {
     var answer = new Array(photo.length).fill(0);
-    for(let i = 0; i<photo.length; i++){
-        for(person of photo[i]){
-            if(name.indexOf(person) > -1) answer[i] += yearning[name.indexOf(person)]; 
-        }
+    
+    for(i in photo){
+        photo[i].forEach((val, idx) => {
+            if(name.indexOf(val) > -1) answer[i] += yearning[name.indexOf(val)];
+        })
     }
     return answer;
 }
