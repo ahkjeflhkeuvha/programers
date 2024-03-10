b46f6641c3afcccf1fc58a263f780089130fe753
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-10 18:50:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 78.8 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 10일 18:49:08
+2024년 03월 10일 18:50:03
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(String s) {
         String strArr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i = 0; i<strArr.length; i++){
-            if(s.contains(strArr[i])) s = s.replace(strArr[i], Integer.toString(i));
-        }
+        for(int i = 0; i<strArr.length; i++) s = s.replace(strArr[i], Integer.toString(i));
         return Integer.parseInt(s);
     }
 }
