526a25f5602568c4f4f7ca1023c7d80c33adb1e5
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-20 19:34:34 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(numbers: IntArray): Double {
+        var sum: Double = 0.0
+        
+        for(i in numbers) sum += i
+        return sum/numbers.size
+    }
+}
