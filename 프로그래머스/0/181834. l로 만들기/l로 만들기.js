7389bea2e864df8283a39661bce8187b31ee9578
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-03-26 22:18:23 +0900

    [level 0] Title: l로 만들기, Time: 7.94 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,8 @@
+function solution(myString) {
+    var answer = '';
+    for(let i = 0; i<myString.length; i++){
+        if(myString.charAt(i) < 'l') answer += 'l'
+        else answer += myString.charAt(i)
+    }
+    return answer;
+}
