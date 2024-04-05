955e191d4f460003fcb2ebcfa4d23ae05a37c6d0
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-04-05 17:27:44 +0900

    [level 1] Title: 문자열 나누기, Time: 8.23 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,22 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        int answer = 0, first = 0, last = 0, n = 0;
+        int answer = 0, cnt = 0, n = 0;
         String strArr[] = s.split("");
             
         
         for(int i = 0; i<strArr.length; i++){
-            if(strArr[n].equals(strArr[i])) first++;
-            else last++;
+            if(strArr[n].equals(strArr[i])) cnt++;
+            else cnt--;
             
-            if(first == last) {
+            if(cnt == 0) {
                 answer++;
-                first = 0;
-                last = 0;
                 n = i + 1;
             }
             
-            if(i == strArr.length - 1 && (first > 0 || last > 0)) answer++;
+            if(i == strArr.length - 1 && cnt > 0) answer++;
         }
         return answer;
     }
