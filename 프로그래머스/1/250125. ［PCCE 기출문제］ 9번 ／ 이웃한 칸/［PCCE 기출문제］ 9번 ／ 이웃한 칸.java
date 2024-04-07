b60022f71bfad0fdc4466232540a5322bc04551e
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-04-07 19:07:46 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,16 +1,18 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
-        String color = board[h][w];
-        int direction[][] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        int n = board.length;
         
-        for(int dir[] : direction){
-            int mov_h = h + dir[0];
-            int mov_w = w + dir[1];
+        int dh[] = {0, 1, -1, 0};
+        int dw[] = {1, 0, 0, -1};
+        
+        for(int i = 0; i<dh.length; i++){
+            int h_check = h + dh[i];
+            int w_check = w + dw[i];
             
-             if ((mov_h >= 0) && (mov_h < board.length) && (mov_w >= 0) && (mov_w < board[0].length)){
-                if (color.equals(board[mov_h][mov_w])) answer += 1;
-             }
+            if((h_check >= 0 && h_check < n) && (w_check >= 0 && w_check < n)){
+                if(board[h][w].equals(board[h_check][w_check])) answer++;
+            }
         }
         return answer;
     }
