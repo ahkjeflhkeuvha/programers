f49fe0407dc83201d704de2c1b101b519cf6afd3
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-04-10 20:27:15 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,28 +1,21 @@
-import java.util.HashMap;
-
-public class Solution {
+import java.util.*;
+class Solution {
     public String[] solution(String[] players, String[] callings) {
-        int n = players.length;
-        HashMap<String, Integer> indexMap = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            indexMap.put(players[i], i);
-        }
-
-        for (String calling : callings) {
-            int idx = indexMap.get(calling);
-            // 여기는 return하기 위해서!
+        HashMap<String, Integer> player = new HashMap<>();
+        
+        for(int i = 0; i<players.length; i++) player.put(players[i], i);
+        
+        for(String call : callings){
+            int idx = player.get(call);
+            
             String temp = players[idx - 1];
             players[idx - 1] = players[idx];
             players[idx] = temp;
-
-            // 이렇게 하는 이유! map에 이름과 순위를 저장 -> map에 변화가 없으면 idx에 계속 처음과 같은 값이 들어감 (idx를 map에서 가져옴)
-            // 따라서 순위가 변함에 따라 map의 순위도 변경
             
-            indexMap.put(players[idx - 1], idx - 1);
-            indexMap.put(players[idx], idx);
+            player.put(players[idx - 1], idx - 1);
+            player.put(players[idx], idx);
         }
-
+        
         return players;
     }
 }
