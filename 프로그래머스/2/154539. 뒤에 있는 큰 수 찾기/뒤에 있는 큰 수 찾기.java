bba753d03fcca1bdfa3263d8ab6621ae6c3523a3
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-05-15 22:03:42 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 110.75 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -3,35 +3,23 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
 
-        // -1로 원소 채우기
-        int[] result = new int[numbers.length];
-        Arrays.fill(result, -1);
-
-        // 스택
         Stack<Integer> stack = new Stack<Integer>();
-
-        // 뒤에서부터 검사
-        for(int i = numbers.length-1; i>-1; i--){
-
-            // stack이 비어있지 않고
-            // 현재 숫자(numbers[i])가 stack의 맨 위 원소(stack.peek())보다 크거나 같으면
-            // 현재 숫자(numbers[i])가 뒤에 숫자보다 크므로 stack 맨 위 원소 삭제
-            while (!stack.empty() && numbers[i] >= stack.peek()){
-                stack.pop();
+        stack.push(0);
+        
+        for(int i = 1; i<numbers.length; i++){
+            while(!stack.isEmpty()){
+                int idx = stack.pop();
+                if(numbers[i] > numbers[idx]) answer[idx] = numbers[i];
+                else {
+                    stack.push(idx);
+                    break;
+                }
             }
-
-            // stack이 비어있지 않은 경우
-            // 현재 숫자(numbers[i])의 바로 뒤에 큰 수를 찾았으므로
-            // result[i]에 대입
-            if(!stack.empty()){
-                result[i] = stack.peek();
-            }
-
-            // stack에 현재 숫자(numbers[i]) 추가
-            stack.push(numbers[i]);
+            stack.push(i);
         }
-
-        return result;
+        return answer;
     }
 }
