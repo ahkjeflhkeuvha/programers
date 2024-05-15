3e4dbcc7ff147ebef709fce9831719586c68d67f
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-05-15 21:43:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.78 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,33 +1,33 @@
-import java.util.Collections;
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = {0,0};
-        ArrayList <Integer>list = new ArrayList<Integer>(10);
-        
+        PriorityQueue<Integer> max = new PriorityQueue<>();
+        PriorityQueue<Integer> min = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0; i<operations.length; i++){
-            String st[] = operations[i].split(" ");
-            if(st[0].equals("I")) list.add(Integer.parseInt(st[1]));
-            else if(st[0].equals("D") && st[1].equals("1")){
-                Collections.sort(list);
-                if(list.size() == 0) continue;
-                else list.remove(list.size() - 1);
+            String strArr[] = operations[i].split(" ");
+            
+            if(strArr[0].equals("I")){
+                max.add(Integer.parseInt(strArr[1]));
+                min.add(Integer.parseInt(strArr[1]));
             }
-            else if(st[0].equals("D") && st[1].equals("-1")){
-                Collections.sort(list);
-                if(list.size() == 0) continue;
-                else list.remove(0);
-            }   
-        }
-        Collections.sort(list);
-        if(list.size() == 0) return answer;
-        else {
-            answer[0] = list.get(list.size() - 1);
-            answer[1] = list.get(0);
+            // max는 최대값이 첫 번째에 min은 최소값이 첫 번째에
+            else if(max.size() > 0) {
+                if(strArr[1].equals("1")) max.remove(min.poll());
+                else min.remove(max.poll());
+            }
         }
         
+        int[] answer = new int[2];
+        if(max.size() == 0) {
+            answer[0] = 0;
+            answer[1] = 0;
+        }
+        else {
+            answer[0] = min.poll();
+            answer[1] = max.poll();
+        }
+ 
         return answer;
     }
 }
