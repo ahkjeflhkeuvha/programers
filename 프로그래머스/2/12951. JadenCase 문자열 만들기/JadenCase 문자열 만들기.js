22a76f0151d0ac75b397f9ccd8619a507efe87d4
오지은 <w2317@e-mirim.hs.kr>
2024-06-22 17:02:20 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,5 @@
 function solution(s) {
-    let strArr = s.split(/\s/g)
+    let strArr = s.split(' ')
     strArr = strArr.map((val) => {
         return val.charAt(0).toUpperCase().concat(val.substring(1).toLowerCase())
     })
