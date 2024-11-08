780da282017de20afcc0e62fce005e8999cd822c
오지은 <w2317@e-mirim.hs.kr>
2024-11-08 18:12:16 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,8 +1,6 @@
 function solution(new_id) {
     var answer = '';
-    answer = new_id.toLowerCase()
-    answer = answer.match(/[a-z0-9-_.]/g).join('')
-    answer = answer.replace(/\.{2,}/g, ".")
+    answer = new_id.toLowerCase().match(/[a-z0-9-_.]/g).join('').replace(/\.+/g, ".")
 
     if(answer.startsWith(".")) answer = answer.substring(1, answer.length)
     else if(answer.endsWith(".")) answer = answer.substring(0, answer.length - 1)
