cf3219a66562d21ddc4db8ac4fdc8907457a0a7d
오지은 <w2317@e-mirim.hs.kr>
2024-11-08 18:11:01 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410?language=javascript) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 3.17 ms
+메모리: 33.5 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 16일 20:53:18
+2024년 11월 08일 18:10:56
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,18 @@
+function solution(new_id) {
+    var answer = '';
+    answer = new_id.toLowerCase()
+    answer = answer.match(/[a-z0-9-_.]/g).join('')
+    answer = answer.replace(/\.{2,}/g, ".")
+
+    if(answer.startsWith(".")) answer = answer.substring(1, answer.length)
+    else if(answer.endsWith(".")) answer = answer.substring(0, answer.length - 1)
+    
+    if(answer.length === 0) answer = "a"
+    else if(answer.length >= 16) answer = answer.substr(0, 15)
+    
+    if(answer.endsWith(".")) answer = answer.substring(0, answer.length - 1)
+    
+    while(answer.length <= 2) answer += answer[answer.length - 1]
+    
+    return answer;
+}
