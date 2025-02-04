c671a83dbf6887e462cac5fc5402cca6816697a4
오지은 <w2317@e-mirim.hs.kr>
2025-02-04 11:30:37 +0900

    [Bronze I] Title: 단어 공부, Time: 288 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -2,9 +2,9 @@ inputStr = input()
 arr = [0 for i in range(26)]
 
 for i in inputStr:
-    arr[ord(i.lower()) - ord('a')] += 1
+    arr[ord(i.upper()) - ord('A')] += 1
 
 sortedArr = sorted(arr, reverse=True)
-answer = '?' if sortedArr[0] == sortedArr[1] else chr(arr.index(max(arr)) + ord('a')).upper()
+answer = '?' if sortedArr[0] == sortedArr[1] else chr(arr.index(max(arr)) + ord('A'))
 
 print(answer)
