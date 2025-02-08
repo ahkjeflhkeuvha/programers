e4900ab62e070463e6592d7227bb33cca7750539
오지은 <w2317@e-mirim.hs.kr>
2025-02-08 19:19:48 +0900

    [level 2] Title: 프로세스, Time: 1.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 def solution(priorities, location):
     answer = 0
     queue = deque()
@@ -6,23 +7,18 @@ def solution(priorities, location):
     for i in range(len(priorities)):
         queue.append([i, priorities[i]])
     
-    
-    co = priorities
     n = 0
     while queue:
         idx, process = queue.popleft()
 
-        if max(co) > process:
+        if any(process < data for data in priorities):
             queue.append([idx, process])
         else:
             print(idx, process)
-            co[idx] = -1
-            n += 1
+            priorities[idx] = -1
+            answer += 1
             
             if idx == location:
                 break
-        
-        
     
-    
-    return n
+    return answer
