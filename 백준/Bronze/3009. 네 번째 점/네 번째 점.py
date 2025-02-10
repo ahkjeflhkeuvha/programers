0a60d2d71c833d7754e8cecd9dcdcd3733ab5f14
오지은 <w2317@e-mirim.hs.kr>
2025-02-10 12:16:05 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,24 +1,17 @@
-x_dic = {}
-y_dic = {}
+x_num = []
+y_num = []
 
 
 for i in range(3):
     x, y = map(int, input().split())
    
-    if x not in x_dic:
-        x_dic[x] = 1
-    else:
-        x_dic[x] += 1
+    x_num.append(x)
+    y_num.append(y)
 
-    if y not in y_dic:
-        y_dic[y] = 1
-    else:
-        y_dic[y] += 1
+for i in range(3):
+    if x_num.count(x_num[i]) == 1:
+        x4 = x_num[i]
+    if y_num.count(y_num[i]) == 1:
+        y4 = y_num[i]
 
-for key in x_dic.keys():
-    if x_dic[key] == 1:
-        print(key, end = " ")
-
-for key in y_dic.keys():
-    if y_dic[key] == 1:
-        print(key, end="")
+print("{} {}".format(x4, y4))
