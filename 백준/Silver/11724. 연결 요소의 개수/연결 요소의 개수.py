d5dc5f180c3f4947c030d7621e2751ea4502ba90
오지은 <w2317@e-mirim.hs.kr>
2025-02-18 10:52:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 672 ms, Memory: 39900 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,38 @@
+import sys
+
+N, M = map(int, input().split())
+
+graph = [[0 for _ in range(N)] for _ in range(N)]
+
+for _ in range(M):
+    x, y = map(int, sys.stdin.readline().strip().split())
+
+    graph[x-1][y-1] = 1
+    graph[y-1][x-1] = 1
+
+visited = [False]*N
+cnt = 0
+
+def dfs(graph, start):
+    global visited 
+    global cnt
+
+    stack = [start]
+    visited[start] = True
+    cnt += 1
+
+    while stack:
+        node = stack.pop()
+        for j in range(N):
+            if graph[node][j] == 1 and not visited[j]:
+                visited[j] = True
+                stack.append(j)
+
+
+dfs(graph, 0)
+
+for i in range(N):
+    if visited[i] == False:
+        dfs(graph, i)
+
+print(cnt)
