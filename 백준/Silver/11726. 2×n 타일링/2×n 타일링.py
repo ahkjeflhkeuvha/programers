21b4ef0aa70430ca6e283bbd03c85ea69e3b742f
오지은 <w2317@e-mirim.hs.kr>
2025-02-20 10:23:17 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,18 +1,8 @@
-import sys
-sys.setrecursionlimit(60000)
-
 N = int(input())
 
-case_arr = [-1 for _ in range(60001)]
+case_arr = [1, 1, 2]
 
-def dp(N):
-    if case_arr[N] != -1:
-        return case_arr[N]
-    if N == 0:
-        return 1
-    if N == 1:
-        return 1
-    case_arr[N] = (dp(N-1) + dp(N-2)) % 10007
-    return case_arr[N]
+while len(case_arr) <= N:
+    case_arr.append((case_arr[-1] + case_arr[-2]) % 10007)
 
-print(dp(N))
+print(case_arr[N])
