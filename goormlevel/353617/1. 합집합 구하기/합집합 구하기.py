93d78c9896f3204f2fdde4a8f0a1ae303d052124
오지은 <w2317@e-mirim.hs.kr>
2025-02-24 09:39:11 +0900

    [난이도 2] Title: 합집합 구하기, Time: 0.03 ms, Memory: 11.10 MB -BaekjoonHub

---
+++ goormlevel/353617/1. 합집합 구하기/합집합 구하기.py
@@ -0,0 +1,10 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().strip().split())
+
+a = set(list(map(int, sys.stdin.readline().strip().split())))
+b = set(list(map(int, sys.stdin.readline().strip().split())))
+
+c = list(sorted(list(a | b)))
+print(len(c))
+print(*c)
