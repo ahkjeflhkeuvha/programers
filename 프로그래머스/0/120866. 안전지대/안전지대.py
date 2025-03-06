6833e71b08d1bf128f29112175ba7032abe47331
오지은 <w2317@e-mirim.hs.kr>
2025-03-06 21:35:43 +0900

    [level 0] Title: 안전지대, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,16 +1,12 @@
 def solution(board):
     answer = 0
-    dx, dy = [-1, -1, -1, 0, 0, 0, 1, 1, 1], [-1, 0, 1, -1, 0, 1, -1, 0, 1]
     danger_area = set()
     n = len(board)
     
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                danger_area.add((i, j))
-                for k in range(9):
-                    nx, ny = i + dx[k], j + dy[k]
-                    if 0 <= nx < n and 0 <= ny < n:
-                        danger_area.add((nx, ny))
-                        
-    return pow(len(board), 2) - len(danger_area)
+    for (i, row) in enumerate(board):
+        for (j, x) in enumerate(row):
+            if not x == 1:
+                continue
+            danger_area.update((i+nx, j+ny) for nx in [-1, 0, 1] for ny in [-1, 0, 1] if 0 <= i + nx < n and 0 <= j + ny < n)
+
+    return n*n - len(danger_area)
