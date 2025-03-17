3cb158feb122575cd193f3bceba48ea7814e42b7
오지은 <w2317@e-mirim.hs.kr>
2025-03-17 19:30:25 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 146.80 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/README.md
+++ 프로그래머스/2/135807. 숫자 카드 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 카드 나누기 - 135807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807#) 
 
 ### 성능 요약
 
-메모리: 52.5 MB, 시간: 231.09 ms
+메모리: 52.2 MB, 시간: 146.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 19:28:30
+2025년 03월 17일 19:30:23
 
 ### 문제 설명
 
--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,15 +1,11 @@
-import math
+from functools import reduce
+from math import gcd
 
 def solution(arrayA, arrayB):
     answer = 0
-    tempA, tempB = arrayA[0], arrayB[0]
-    for i in range(1, len(arrayA)):
-        tempA = math.gcd(tempA, arrayA[i])
-        tempB = math.gcd(tempB, arrayB[i])
+    tempA, tempB = reduce(gcd, arrayA), reduce(gcd, arrayB)
     
     flagA = all(arrayB[i] % tempA != 0 for i in range(len(arrayB)))
     flagB = all(arrayA[i] % tempB != 0 for i in range(len(arrayA)))
-    
-    print(flagA, tempA, flagB, tempB)
     
     return max(0, tempA if flagA else -1, tempB if flagB else -1)
