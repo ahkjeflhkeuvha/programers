08a0a4dda93706c49dcc1a57d20340a28100830a
오지은 <w2317@e-mirim.hs.kr>
2025-05-11 19:10:31 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,27 +1,21 @@
 function solution(n) {
-    const answer =  Array.from(Array(n), () => Array(n).fill(null));
-    var num = 1, strRow = 0, endRow = n-1, strCol = 0, endCol = n-1;
-    while(num <= n*n){
-        //오위 -> 왼위
-        for(let i = strCol; i<=endCol; i++) 
-            answer[strRow][i] = num++;
-        strRow++;
+    var answer = Array.from(Array(n), () => Array(n).fill(null));
+    let num = 1
+    let [startRow, endRow, startCol, endCol] = [0, n - 1, 0, n - 1]
+    
+    while(num <= Math.pow(n, 2)) {
+        for(let j = startCol; j <= endCol; j++) answer[startRow][j] = num++
+        startRow += 1
         
-        //왼위 -> 왼아래
-        for(let i = strRow; i<=endRow; i++) 
-            answer[i][endCol] = num++;
-        endCol--;
+        for(let i = startRow; i <= endRow; i++) answer[i][endCol] = num++
+        endCol -= 1
         
-        //왼아래 -> 오아래
-        for(let i = endCol; i>=strCol; i--) 
-            answer[endRow][i] = num++;
-        endRow--;
+        for(let j = endCol; j>= startCol; j--) answer[endRow][j] = num++
+        endRow -= 1
         
-        //오아래 -> 오위
-        for(let i = endRow; i>=strRow; i--) 
-            answer[i][strCol] = num++;
-        strCol++;
-        
+        for(let i = endRow; i >= startRow; i--) answer[i][startCol] = num++
+        startCol += 1
     }
+    
     return answer;
 }
